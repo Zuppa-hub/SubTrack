31409c8b86b50e64b670ef3ddf178282e9d268bd
Andrea Cazzato <cazzatoandrea@protonmail.com>
2025-12-07 20:34:05 +0100

    Initial Commit

---
+++ SubTrack/SubTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubTrackApp.swift
+//  SubTrack
+//
+//  Created by Andrea Cazzato on 07/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SubTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
